0ddb650841d56288ea3998ab7e00372ade758a90
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-03-23 20:17:28 +0530

    shows only active elements

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const btns = document.querySelectorAll('.tab-btn');
 const about = document.querySelector('.about');
-const content = document.querySelector('.content');
+const content = document.querySelectorAll('.content');
 
+about.addEventListener('click', (e)=>{
+    const id = e.target.dataset.id;
+    if(id){
+        // Remove active class
+        btns.forEach((btn)=>{
+            btn.classList.remove('active');
+            e.target.classList.add('active');
+        });
 
+        content.forEach((article)=>{
+            article.classList.remove('active')
+        });
+
+        const info = document.getElementById(id);
+        info.classList.add('active');
+    }
+});
